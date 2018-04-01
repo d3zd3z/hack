a68f5418d90239e662a62f5319cf9131fe2c722c
David Brown <david.brown@linaro.org>
2018-04-01 11:27:31 -0600

    Add snapshot naming example
    Make sure we understand time formatting for snapshots.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,10 +2,18 @@ module Lib
     ( someFunc
     ) where
 
-import Data.List (intercalate)
 import Hack.Zfs
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Format (formatTime, FormatTime, defaultTimeLocale)
 
 someFunc :: IO ()
 someFunc = do
    zs <- getZfs
-   putStrLn $ intercalate "\n" $ map show $ zfsEntries zs
+   now <- getCurrentTime
+   putStrLn $ show $ length $ zfsEntries zs
+   putStrLn $ "snap: " ++ snapName "prefix" now
+
+-- Using the given formattable time, and the given prefix, return a
+-- snapshot name.
+snapName :: FormatTime t => String -> t -> String
+snapName prefix now = prefix ++ "-" ++ formatTime defaultTimeLocale "%0Y%m%d%H%M" now
