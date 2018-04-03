533c1945bde1af7a1531de4f18e0589d1c99d62f
David Brown <david.brown@linaro.org>
2018-04-03 12:08:08 -0600

    Build up a basic command line
    Using optparse-applicative to build up a basic command line, ready to
    handle the snapshot command.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,58 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
-import Lib
+-- import Lib
+
+import Options.Applicative
+import Data.Semigroup ((<>))
 
 main :: IO ()
-main = someFunc
+main = do
+   op <- execParser mainopts
+   putStrLn $ show op
+   case op of
+      AllFlags{..} ->
+         case afCommand of
+            Snap pretend -> putStrLn $ "Snap: " ++ show pretend
+
+data AllFlags = AllFlags {
+   afGlobal :: GlobalFlags,
+   afCommand :: Commands }
+   deriving Show
+
+data GlobalFlags = GlobalFlags {
+   gfConfig :: String }
+   deriving Show
+
+data Commands =
+   Snap Bool
+   deriving Show
+
+mainopts :: ParserInfo AllFlags
+mainopts = info (allopts <**> helper)
+   (fullDesc
+   <> progDesc "Various backup things"
+   <> header "hack - haskell backups stuff")
+
+allopts :: Parser AllFlags
+allopts =
+   AllFlags <$> globalopt <*> subcmd
+
+subcmd :: Parser Commands
+subcmd = subparser (
+   command "snap" (info (snapopts <**> helper) idm))
+
+globalopt :: Parser GlobalFlags
+globalopt =
+   GlobalFlags <$> (
+   strOption (short 'c' <>
+      long "config" <>
+      metavar "CONFIG" <>
+      value "~/.gack.yaml" <>
+      help "Set the config file to use"))
+
+snapopts :: Parser Commands
+snapopts = Snap <$>
+   switch (short 'n' <>
+      long "pretend" <>
+      help "Don't actually run, show what would have been done")
