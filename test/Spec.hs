301d3426139ab45c714b50bbcc486fe1c6d57e8b
David Brown <david.brown@linaro.org>
2018-04-30 09:03:07 -0600

    Add basic unit test to main package

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,30 @@
+-- Test suite
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.ByteString.Lazy as L
+import qualified Data.Aeson as A
+import Data.ByteString (ByteString)
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Data.Weave.Header (Header)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain unitTests
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests"
+    [
+        testCase "Header Test" testHeader ]
+
+testHeader :: IO ()
+testHeader = do
+    let lt1 = L.fromStrict t1
+    enc <- maybe (assertFailure "Decode failure") return $
+        ((A.decode lt1) :: Maybe Header)
+    let dec = A.encode enc
+    lt1 @=? dec
+
+t1 :: ByteString
+t1 = "{\"version\":1,\"deltas\":[{\"name\":\"2018-04-06T14:38:47.973363784-06:00\",\"number\":1,\"tags\":{\"dir\":\"/home/davidb/back/hack\"},\"time\":\"2018-04-06T20:38:48.189712199Z\"}]}"
