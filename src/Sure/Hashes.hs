74cb3ff63be32b33a35b29f80c3affe47adffb2d
David Brown <david.brown@linaro.org>
2018-05-02 08:59:52 -0600

    Clean up hash estimates
    Move some predicates into the Sure.Types package, as they will be
    generally useful, and remove the now conflicting old way that kept the
    whole tree in memory.

--- src/Sure/Hashes.hs
+++ src/Sure/Hashes.hs
@@ -5,14 +5,9 @@
 
 module Sure.Hashes (
     HashProgress(..),
-    estimateHashes,
-    treeFiles, needsHash
+    estimateHashes
 ) where
 
-import qualified Data.ByteString.Char8 as B
-import qualified Data.Map.Strict as Map
-import Data.Map.Strict ((!), notMember)
-import qualified Data.Vector as V
 import Pipes
 import qualified Pipes.Prelude as PP
 
@@ -25,17 +20,11 @@ estimateHashes :: Monad m => Producer SureNode m () -> m HashProgress
 estimateHashes = PP.fold (flip updateProgress) hashProgress0 id
 
 updateProgress :: SureNode -> HashProgress -> HashProgress
-updateProgress (SureNode _ atts) hp =
-    if
-        Map.lookup "kind" atts /= Just "file" ||
-        Map.member "sha1" atts then hp
-    else hp {
+updateProgress node hp =
+    if needsHash node then hp {
         hpTotalFiles = hpTotalFiles hp + 1,
-        hpTotalBytes = hpTotalBytes hp + size }
-    where
-        size :: Integer
-        size = maybe 0 id $ fmap (read . B.unpack) $ Map.lookup "size" atts
-updateProgress _ hp = hp
+        hpTotalBytes = hpTotalBytes hp + nodeSize node }
+    else hp
 
 data HashProgress = HashProgress {
     hpTotalFiles :: Integer,
@@ -46,17 +35,3 @@ data HashProgress = HashProgress {
 
 hashProgress0 :: HashProgress
 hashProgress0 = HashProgress 0 0 0 0
-
--- |Estimate the work needed to update hashes.  We consider any file
--- nodes that do not have a 'sha1' field as needing hashes.
-
--- |Walk through a tree, returning all of the file nodes.
-treeFiles :: SureTree -> [SureFile]
-treeFiles SureTree{..} =
-    concatMap treeFiles (V.toList stChildren) ++
-    V.toList stFiles
-
--- |Does this file node need a hash computed on it.
-needsHash :: SureFile -> Bool
-needsHash f = atts ! "kind" == "file" && notMember "sha1" atts
-    where atts = sfAtts f
--- src/Sure/Types.hs
+++ src/Sure/Types.hs
@@ -1,14 +1,22 @@
 -- | Types for sure files
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Sure.Types (
    AttMap,
    SureTree(..),
    SureFile(..),
-   SureNode(..)
+   SureNode(..),
+
+   isFile,
+   needsHash,
+   nodeSize
 ) where
 
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Vector (Vector)
 
 -- |An attribute maintains a k/v mapping for the filesystem mappings.
@@ -36,3 +44,22 @@ data SureNode =
     SureSep |
     SureNode !B.ByteString !AttMap
     deriving Show
+
+-- |Indicate if this node represents a regular file.
+isFile :: SureNode -> Bool
+isFile (SureNode _ atts) = Map.lookup "kind" atts == Just "file"
+isFile _                 = False
+
+-- |Determine if this node needs a hash computation.  This is for
+-- regular files that don't have a "sha1" property.
+needsHash :: SureNode -> Bool
+needsHash (SureNode _ atts) =
+    Map.lookup "kind" atts == Just "file" &&
+        Map.notMember "sha1" atts
+needsHash _                 = False
+
+-- |Return the size of this node.  Returns '0' if there is no size
+-- property, or if the node isn't a regular node.
+nodeSize :: SureNode -> Integer
+nodeSize (SureNode _ atts) = maybe 0 id $ fmap (read . C8.unpack) $ Map.lookup "size" atts
+nodeSize _                 = 0
